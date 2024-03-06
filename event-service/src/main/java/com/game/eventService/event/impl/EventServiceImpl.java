04ecbe7a04c400150d1598813227e491c15b16fc
Jacobs <jakub.banach72@gmail.com>
2024-03-06 18:05:04 +0100

    Adding basic Event methods

--- event-service/src/main/java/com/game/eventService/event/impl/EventServiceImpl.java
+++ event-service/src/main/java/com/game/eventService/event/impl/EventServiceImpl.java
@@ -5,5 +5,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 class EventServiceImpl implements EventService {
+    // TODO: Reactive vs NOT
+    void addEvent(){
 
+    }
+
+    void getEvent(){
+
+    }
+
+    void getAllEvents(){
+
+    }
+
+    void deleteEvent(){
+
+    }
+
+    void deleteAllEvents(){
+
+    }
 }
